07282e7ed360c40b7d9d291e09194ce2747f133e
Vezhdel Vasyl <vezhdelit@gmail.com>
2023-11-25 02:03:16 +0200

    update: update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,8 @@ import Box from "@/components/ui/box";
 
 const Home = () => {
   return (
-    <section className=" h-full flex-1 flex-col">
+    <section className="flex h-full flex-1 flex-col  gap-5">
+      <Box className=" h-fit w-full rounded-lg  p-4">Home</Box>
       <Box className=" h-full w-full rounded-lg  p-4">Home</Box>
     </section>
   );
